9ae673299810be95490793788f7084c34b764e74
fgabrielqr <fgabrielqr@gmail.com>
2024-10-21 13:12:11 -0300

    feat: :rocket: subindo projeto

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-})
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    historyApiFallback: true,  // Adicione essa linha
+  },
+});
